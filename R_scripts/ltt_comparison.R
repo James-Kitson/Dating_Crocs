4c6adf6a0a60f3129e7c1bb938c938491ce8bab3
James-Kitson <jameskitson10@gmail.com>
2016-04-07 14:10:36 +0100

    reformatted this script to plot all the lines on the same plot.

--- R_scripts/ltt_comparison.R
+++ R_scripts/ltt_comparison.R
@@ -36,7 +36,7 @@ tree.constrained215<-Bayes.tree.constrained215[[1]]
 tree.constrained215<-ladderize(tree.constrained215)
 tree.constrained215<-root(tree.constrained215,1)
 ### scale the edge lengths to the root age of the tree
-tree.constrained215$edge.length<-tree.constrained201$edge.length/(max(nodeHeights(tree.constrained201)/227))
+tree.constrained215$edge.length<-tree.constrained215$edge.length/(max(nodeHeights(tree.constrained215)/227))
 ### set a root age in the tree object for axisPhylo.
 tree.constrained215$root.time<-227
 
@@ -45,16 +45,9 @@ tree.unconstrained<-Bayes.tree.unconstrained[[1]]
 tree.unconstrained<-ladderize(tree.unconstrained)
 tree.unconstrained<-root(tree.unconstrained,1)
 ### scale the edge lengths to the root age of the tree
-tree.unconstrained$edge.length<-tree.constrained201$edge.length/(max(nodeHeights(tree.constrained201)/227))
+tree.unconstrained$edge.length<-tree.unconstrained$edge.length/(max(nodeHeights(tree.unconstrained)/227))
 ### set a root age in the tree object for axisPhylo.
 tree.unconstrained$root.time<-227
-
-### make an offset for the axis as R won't draw it from the tip to the root. The offset is a negative starting point for the axis equivalent to the
-### rounding up we do at the root end of the axis i.e. if we round 227 Mya to 230 Mya then we need to offset by minus 3Ma of distance measured in
-### branch lengths. To do this we divide the root height by the root age and multiply by -3.
-offset.unconstrained<-3*(max(nodeHeights(tree.unconstrained)/227))
-offset.constrained201<-3*(max(nodeHeights(tree.constrained201)/227))
-offset.constrained215<-3*(max(nodeHeights(tree.constrained215)/227))
 
 ### Read in the geological epoch data
 epochs<-read.csv("Data/Metadata/epochs_and_colours.csv", stringsAsFactors = F)
@@ -76,39 +69,18 @@ legend.cols<-rgb(red=epochs$Red, green=epochs$Green, blue=epochs$Blue, alpha=125
 
 ## @knitr lttcomparisonplot
 
-par(mfrow=c(3,1),
-    mai=c(1, 1, 0.2, 1))
-
 ### plot the unconstrained ltt
-ltt.plot(tree.unconstrained, xlab="Time (Ma)", ylab="Extant lineages", ylim=c(0,70))
+ltt.plot(tree.unconstrained, xlab="Time (Ma)", ylab="Extant lineages", ylim=c(0,70), lty=1)
+ltt.lines(tree.constrained215, lty=2)
+ltt.lines(tree.constrained201, lty=3)
 title(main="Thallatosuchian root unconstrained")
 ### plot the epochs
 for(i in 1:length(epochs$Starting)){
   polygon(x=c(epochs$unconstrained.start[i],epochs$unconstrained.start[i],epochs$unconstrained.end[i],epochs$unconstrained.end[i]),
           y = c(0,70,70,0), border=NA, col =legend.cols[i])
 }
-#axis(side=1,cex.axis=0.8, padj=1,at=seq(from=-(max(nodeHeights(tree.unconstrained)+offset.unconstrained)), to=0, by=(max(nodeHeights(tree.unconstrained))+offset.unconstrained)/23), labels=seq(230,0,by=-10))
+
 legend("topright", title="Epoch", inset=0.005, legend = epochs$Stage,
        fill =legend.cols,
        cex=0.5,
        bg = "white")
-
-### plot the ltt for the analysis constrained at 201Ma
-ltt.plot(tree.constrained201, xlab="Time (Ma)", ylab="Extant lineages", ylim=c(0,70))
-title(main="Thallatosuchian root constrained between 191Ma and 201Ma")
-for(i in 1:length(epochs$Starting)){
-  polygon(x=c(epochs$constrained201.start[i],epochs$constrained201.start[i],epochs$constrained201.end[i],epochs$constrained201.end[i]),
-          y = c(0,70,70,0), border=NA, col =legend.cols[i])
-}
-#axis(side=1,cex.axis=0.8, padj=1,at=seq(from=-(max(nodeHeights(tree.constrained201)+offset.constrained201)), to=0, by=(max(nodeHeights(tree.constrained201))+offset.constrained201)/23), labels=seq(230,0,by=-10))
-
-### plot the ltt for the analysis constrained at 215Ma
-ltt.plot(tree.constrained215, xlab="Time (Ma)", ylab="Extant lineages", ylim=c(0,70))
-title(main="Thallatosuchian root constrained between 205Ma and 215Ma")
-for(i in 1:length(epochs$Starting)){
-  polygon(x=c(epochs$constrained215.start[i],epochs$constrained215.start[i],epochs$constrained215.end[i],epochs$constrained215.end[i]),
-          y = c(0,70,70,0), border=NA, col =legend.cols[i])
-}
-#axis(side=1,cex.axis=0.8, padj=1,at=seq(from=-(max(nodeHeights(tree.constrained215)+offset.constrained215)), to=0, by=(max(nodeHeights(tree.constrained215))+offset.constrained215)/23), labels=seq(230,0,by=-10))
-
-#dev.off()
