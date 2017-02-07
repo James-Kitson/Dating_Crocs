43cd5cf857464321bbb636299df3984e67b79055
James Kitson <njk72@newcastle.ac.uk>
2017-02-07 11:13:37 +0000

    going right down to only allowing less than 10% of missing data doesn't give many taxa so I've chenged the percentages but kept the same number of slices

--- R_scripts/Subsetting_for_sensitivity_analysis.R
+++ R_scripts/Subsetting_for_sensitivity_analysis.R
@@ -13,14 +13,14 @@ ages<-read.csv("data/sensitivity_testing/Taxon_ages.csv", stringsAsFactors = FAL
 ages$constraint<-paste(ages$Taxa," = fixed(",ages$Age,")", sep="")
 
 #subset the taxa by amount of missing data and write taxa matricies
-for(i in c(10,20,30,40,50)){
-  trimmed<-subset(characters,Percentage_missing<i, select=c("Species","Characters"))
+for(i in c(30,40,50,60,70)){
+  trimmed<-subset(characters,Percentage_missing<=i, select=c("Species","Characters"))
   write.table(trimmed, file=paste("data/sensitivity_testing/Subset_characters_",i,".txt",sep=""), sep=" ", col.names = FALSE, row.names = FALSE)
 }
 
 #subset the age metadata to equivalent species in each subset of taxa
-for(i in c(10,20,30,40,50)){
-  trimmed_characters<-subset(characters,Percentage_missing<i, select=c("Species","Characters"))
+for(i in c(30,40,50,60,70)){
+  trimmed_characters<-subset(characters,Percentage_missing<=i, select=c("Species","Characters"))
   trimmed_ages<-ages[match(trimmed_characters$Species,ages$Taxa),]
   write.csv(trimmed_ages, file=paste("data/sensitivity_testing/Subset_ages_",i,".csv",sep=""), row.names = FALSE)
 }
