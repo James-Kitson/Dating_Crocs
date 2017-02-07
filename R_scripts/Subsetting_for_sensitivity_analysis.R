5cde3a66640cab9c68d67061e0fecbe037aef4f1
James Kitson <njk72@newcastle.ac.uk>
2017-02-07 10:54:28 +0000

    wrote a bit of code that automatically generates the fixed contrstraint commands for MrBayes and also fixed my subsetting as I was generating progressively larger and worse datasets instead of progressively smaller and better datasets.

--- R_scripts/Subsetting_for_sensitivity_analysis.R
+++ R_scripts/Subsetting_for_sensitivity_analysis.R
@@ -9,13 +9,18 @@ rm(list=ls())
 characters<-read.csv("data/sensitivity_testing/Data_completeness.csv", stringsAsFactors = FALSE)
 ages<-read.csv("data/sensitivity_testing/Taxon_ages.csv", stringsAsFactors = FALSE)
 
+#write the basic age constraint comands, will need a bit of editing when we combine this in to the nexus file for Mrbayes
+ages$constraint<-paste(ages$Taxa," = fixed(",ages$Age,")", sep="")
+
+#subset the taxa by amount of missing data and write taxa matricies
 for(i in c(10,20,30,40,50)){
-  trimmed<-subset(characters,Percentage_missing>i, select=c("Species","Characters"))
+  trimmed<-subset(characters,Percentage_missing<i, select=c("Species","Characters"))
   write.table(trimmed, file=paste("data/sensitivity_testing/Subset_characters_",i,".txt",sep=""), sep=" ", col.names = FALSE, row.names = FALSE)
 }
 
+#subset the age metadata to equivalent species in each subset of taxa
 for(i in c(10,20,30,40,50)){
-  trimmed_characters<-subset(characters,Percentage_missing>i, select=c("Species","Characters"))
+  trimmed_characters<-subset(characters,Percentage_missing<i, select=c("Species","Characters"))
   trimmed_ages<-ages[match(trimmed_characters$Species,ages$Taxa),]
   write.csv(trimmed_ages, file=paste("data/sensitivity_testing/Subset_ages_",i,".csv",sep=""), row.names = FALSE)
 }
